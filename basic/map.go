bd69822633faca511291e0b4fac7bfb6982b7931
Han Zhang <Stephenzhang0713@outlook.com>
2022-04-12 16:03:19 +0800

    commit

--- basic/map.go
+++ basic/map.go
@@ -25,4 +25,16 @@ func main() {
 	} else {
 		fmt.Println("查无此人")
 	}
+
+	var sliceMap = make(map[string][]string, 3)
+	fmt.Println(sliceMap)
+	fmt.Println("after init")
+	key := "中国"
+	value, ok := sliceMap[key]
+	if !ok {
+		value = make([]string, 0, 2)
+	}
+	value = append(value, "北京", "上海")
+	sliceMap[key] = value
+	fmt.Println(sliceMap)
 }
